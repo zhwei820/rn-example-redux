dfa14e56cdc0b1af05345808c1d36b06dac3544b
Your Name <you@example.com>
2016-06-18 12:37:01 +0800

    commit

--- src/screens/FirstTabScreen0.js
+++ src/screens/FirstTabScreen0.js
@@ -5,83 +5,64 @@ import {
   ScrollView,
   TouchableOpacity,
   StyleSheet,
-  TextInput,
   Alert
 } from 'react-native';
 import { connect } from 'react-redux';
 import * as counterActions from '../reducers/counter/actions';
 
 // this is a traditional React component connected to the redux store
-class PushedScreen extends Component {
+class FirstTabScreen extends Component {
   static navigatorStyle = {
     statusBarColor: '#303F9F',
     toolBarColor: '#3F51B5',
     navigationBarColor: '#303F9F',
     tabSelectedTextColor: '#FFA000',
     tabNormalTextColor: '#FFC107',
-    tabIndicatorColor: '#FF4081'
+    tabIndicatorColor: '#FFA000'
   };
 
-  static propTypes = {
-    str: PropTypes.string.isRequired,
-    obj: PropTypes.object.isRequired,
-    num: PropTypes.number.isRequired
+  static navigatorButtons = {
+    rightButtons: [
+      {
+        title: 'Edit',
+        id: 'edit'
+      },
+      {
+        icon: require('../../img/navicon_add.png'),
+        title: 'Add',
+        id: 'add'
+      }
+    ]
   };
 
   constructor(props) {
     super(props);
-    this.bgColor = this.getRandomColor();
+    // if you want to listen on navigator events, set this up
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
-
   }
 
+  onNavigatorEvent(event) {
+    switch (event.id) {
+      case 'edit':
+        Alert.alert('NavBar', 'Edit button pressed');
+        break;
 
-    static navigatorButtons = {
-      rightButtons: [
-        {
-          title: 'Edit1',
-          id: 'edit_1'
-        },
-        {
-          icon: require('../../img/navicon_add.png'),
-          title: 'Add',
-          id: 'add'
-        }
-      ]
-    };
+      case 'add':
+        Alert.alert('NavBar', 'Add button pressed');
+        break;
 
-
-    onNavigatorEvent(event) {
-      switch (event.id) {
-        case 'edit':
-          Alert.alert('NavBar', 'Edit button pressed');
-          break;
-
-        case 'add':
-          Alert.alert('NavBar', 'Add button pressed');
-          break;
-
-        default:
-          console.log('Unhandled event ' + event.id);
-          break;
-      }
+      default:
+        console.log('Unhandled event ' + event.id);
+        break;
     }
-
-  getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
   }
 
   render() {
     return (
-      <View style={{flex: 1, padding: 20, backgroundColor: this.bgColor}}>
+      <View style={{flex: 1, padding: 20}}>
 
         <Text style={styles.text}>
-          <Text style={{fontWeight: '500'}}>Counter: </Text> {this.props.counter.count}
+          <Text style={{fontWeight: '500'}}>Same Counter: </Text> {this.props.counter.count}
         </Text>
 
         <TouchableOpacity onPress={ this.onIncrementPress.bind(this) }>
@@ -89,36 +70,23 @@ class PushedScreen extends Component {
         </TouchableOpacity>
 
         <TouchableOpacity onPress={ this.onPushPress.bind(this) }>
-          <Text style={styles.button}>Push Another</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={ this.onPopPress.bind(this) }>
-          <Text style={styles.button}>Pop Screen</Text>
+          <Text style={styles.button}>Push Screen</Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={ this.onShowModalPress.bind(this) }>
           <Text style={styles.button}>Modal Screen</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={ this.onDismissModal.bind(this) }>
-          <Text style={styles.button}>Dismiss modal</Text>
-        </TouchableOpacity>
-
-
-        <TouchableOpacity onPress={ this.onDismissAllModalsPress.bind(this) }>
-          <Text style={styles.button}>Dismiss all modals</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={ this.onPopToRootPress.bind(this) }>
-          <Text style={styles.button}>Pop to root</Text>
-        </TouchableOpacity>
-
-        <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}}/>
-
         <Text style={{fontWeight: '500'}}>String prop: {this.props.str}</Text>
         <Text style={{fontWeight: '500'}}>Number prop: {this.props.num}</Text>
         <Text style={{fontWeight: '500'}}>Object prop: {this.props.obj.str}</Text>
         <Text style={{fontWeight: '500'}}>Array prop: {this.props.obj.arr[0].str}</Text>
+
+
+
+        <TouchableOpacity onPress={ this.onShowViewPager.bind(this) }>
+          <Text style={styles.button}>ViewPager Screen</Text>
+        </TouchableOpacity>
 
       </View>
     );
@@ -133,7 +101,7 @@ class PushedScreen extends Component {
       title: "More",
       screen: "example.PushedScreen",
       passProps: {
-        passed: 'This is a prop passed in \'navigator.push()\'!',
+        str: 'This is a prop passed in \'navigator.push()\'!',
         obj: {
           str: 'This is a prop passed in an object!',
           arr: [
@@ -145,10 +113,6 @@ class PushedScreen extends Component {
         num: 1234
       }
     });
-  }
-
-  onPopPress() {
-    this.props.navigator.pop();
   }
 
   onShowModalPress() {
@@ -156,7 +120,7 @@ class PushedScreen extends Component {
       title: "Modal Screen",
       screen: "example.PushedScreen",
       passProps: {
-        passed: 'This is a prop passed in \'navigator.showModal()\'!',
+        str: 'This is a prop passed in \'navigator.showModal()\'!',
         obj: {
           str: 'This is a prop passed in an object!',
           arr: [
@@ -169,17 +133,24 @@ class PushedScreen extends Component {
       }
     });
   }
+  onShowViewPager(){
+    this.props.navigator.push({
+      title: "More",
+      screen: "example.ViewPagerScreen",
+      passProps: {
+        str: 'This is a prop passed in \'navigator.push()\'!',
+        obj: {
+          str: 'This is a prop passed in an object!',
+          arr: [
+            {
+              str: 'This is a prop in an object in an array in an object!'
+            }
+          ]
+        },
+        num: 1234
+      }
+    });
 
-  onDismissAllModalsPress() {
-    this.props.navigator.dismissAllModals();
-  }
-
-  onDismissModal() {
-    this.props.navigator.dismissModal();
-  }
-
-  onPopToRootPress() {
-    this.props.navigator.popToRoot();
   }
 }
 
@@ -206,4 +177,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(PushedScreen);
+export default connect(mapStateToProps)(FirstTabScreen);
